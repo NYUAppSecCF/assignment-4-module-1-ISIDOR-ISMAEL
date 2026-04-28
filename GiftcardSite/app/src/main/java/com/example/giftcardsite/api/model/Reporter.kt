b181794b3339e769f9770a2470030bdc2701027a
ISIDOR-ISMAEL <it2284@nyu.edu>
2026-04-28 00:54:46 -0400

    Fix grader issues: usesCleartextTraffic=false, remove ACCESS_NETWORK_STATE, gut Reporter.kt

--- GiftcardSite/app/src/main/java/com/example/giftcardsite/api/model/Reporter.kt
+++ GiftcardSite/app/src/main/java/com/example/giftcardsite/api/model/Reporter.kt
@@ -1,21 +1,3 @@
 package com.example.giftcardsite.api.model
 
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
-import android.location.Location
-import android.location.LocationListener
-
-class Reporter(private val token: String) : LocationListener, SensorEventListener {
-    override fun onLocationChanged(location: Location) {
-        // Metric collection removed
-    }
-
-    override fun onSensorChanged(event: SensorEvent?) {
-        // Metric collection removed
-    }
-
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-        return
-    }
-}
+class Reporter(private val token: String)
